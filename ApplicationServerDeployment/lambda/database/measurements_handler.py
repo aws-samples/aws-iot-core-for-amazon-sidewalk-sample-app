22d396f11dc3854dac54574cf8ee8bf5871b40f9
szymonslezak <83120231+szymonslezak@users.noreply.github.com>
2023-02-16 02:19:33 -0800

    Filter out expired measurements (#23)
    * Filter out expired measurements
    
    ---------
    
    Co-authored-by: Szymon Ślęzak <szysla@amazon.com>

--- ApplicationServerDeployment/lambda/database/measurements_handler.py
+++ ApplicationServerDeployment/lambda/database/measurements_handler.py
@@ -36,8 +36,10 @@ class MeasurementsHandler:
         """
         items = []
         try:
+            t_now = int(time.time())
             response = self._table.scan(IndexName='wireless_device_id',
-                                        FilterExpression=Attr('wireless_device_id').eq(wireless_device_id))
+                                        FilterExpression=Attr('wireless_device_id').
+                                        eq(wireless_device_id) & Attr('time_to_live').gte(t_now))
             items.extend(response.get('Items', []))
         except ClientError as err:
             logger.error(f'Error while calling get_all_devices: {err}')
