45e779112cf16764a59ee36731f6dfc3274a8868
Zeal Vagadia <zealvag@amazon.com>
2023-11-23 16:31:12 +0530

    Add the OTA IoTWireless API handler

---
+++ ApplicationServerDeployment/lambda/ota/ota_wireless_api_handler.py
@@ -0,0 +1,56 @@
+# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+import datetime
+import boto3
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class IOTWirelessAPIHandler:
+    """
+    A class that has all the API calls for wireless API's
+    """
+
+    def __init__(self):
+        self._session = boto3.Session()
+        self._wireless_client = self._session.client(service_name='iotwireless', endpoint_url= 'https://api.gamma.us-east-1.iotwireless.iot.aws.dev')
+
+    def delete_fuota_task(self, fuota_task_id: str):
+        return self._wireless_client.delete_fuota_task(
+            Id=fuota_task_id
+        )
+
+    def list_wireless_devices(self, fuota_task_id: str, max_results: int, next_token: str):
+        return self._wireless_client.list_wireless_devices(
+            FuotaTaskId=fuota_task_id,
+            MaxResults=max_results,
+            NextToken=next_token
+        )
+    
+    def list_fuota_tasks(self, max_results: int, next_token: str):
+        return self._wireless_client.list_fuota_tasks(
+            MaxResults=max_results,
+            NextToken=next_token
+        )
+
+    def create_fuota_task(self, s3_uri: str, s3_update_role: str):
+        return self._wireless_client.create_fuota_task(
+            FirmwareUpdateImage=s3_uri,
+            FirmwareUpdateRole=s3_update_role,
+            ProtocolType='Sidewalk'
+        )
+    
+    def associate_wireless_device_with_fuota_task(self, fuota_task_id: str, wireless_device_id: str):
+        return self._wireless_client.associate_wireless_device_with_fuota_task(
+            Id=fuota_task_id,
+            WirelessDeviceId=wireless_device_id
+        )
+    
+    def start_fuota_task(self, fuota_task_id: str, start_time: datetime):
+        return self._wireless_client.start_fuota_task(
+            Id=fuota_task_id,
+            Sidewalk={
+                'StartTime': start_time
+            }
+        )
