852bb7d45791d90bd989a654ece27aff5b7d46c3
Zeal Vagadia <zealvag@amazon.com>
2023-11-28 15:07:56 +0530

    Add the s3 bucket & its policy & s3 permissions to the relevant lamdas

--- ApplicationServerDeployment/lambda/ota/ota_upload_file_lambda_handler.py
+++ ApplicationServerDeployment/lambda/ota/ota_upload_file_lambda_handler.py
@@ -6,27 +6,16 @@ Handles uplinks coming from the Sidewalk Sensor Monitoring Demo Application.
 """
 
 import base64
+import os
 import boto3
 import json
 import traceback
-from datetime import datetime, timezone
 from typing import Final
-
-import time_utils
-from device import Device
-from measurement import Measurement
-
-OTA_S3_BUCKET_NAME: Final = "sidewalk-ota-src"
-
-from device_transfers_handler import DeviceTransfersHandler
-from transfer_tasks_handler import TransferTasksHandler
-
-device_transfers_handler: Final = DeviceTransfersHandler()
-transfer_tasks_handler: Final = TransferTasksHandler()
 
 s3 = boto3.client('s3')
 # TODO - Replace with S3_Client
 
+OTA_S3_BUCKET_NAME: Final = os.environ.get('OTA_S3_BUCKET_NAME')
 
 
 def lambda_handler(event, context):
@@ -64,7 +53,6 @@ def lambda_handler(event, context):
             # Decode base64 content to get the original file content
             decoded_file_content = base64.b64decode(file_content)
             print(f'First 100 characters of the file content: {decoded_file_content[:100]}')
-
             s3.put_object(Body=decoded_file_content, Bucket=OTA_S3_BUCKET_NAME, Key=file_name_with_ext)
 
         return {
