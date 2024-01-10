956e56d7d1f644fec20549c69ad166f677ff560b
Anicar Cabrera <anicarfe@amazon.com>
2024-01-10 20:23:49 -0300

    [GUI] set datePikcer range from now + 6 minutes to now + 48 hours

--- ApplicationServerDeployment/gui/src/Views/FirmwareOta/Components/WirelessDevicesTable/DatePicker.tsx
+++ ApplicationServerDeployment/gui/src/Views/FirmwareOta/Components/WirelessDevicesTable/DatePicker.tsx
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: MIT-0
 
 import { DatePicker as AntdDatePicker, DatePickerProps } from 'antd';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import { useCallback } from 'react';
 
 interface Props {
   onDatePickerChange: (date: DatePickerProps['value']) => void;
@@ -10,33 +11,60 @@ interface Props {
 }
 
 export const DatePicker = ({ onDatePickerChange, dateValue }: Props) => {
-  const range = (start: number, end: number) => {
-    const result = [];
-    for (let i = start; i < end; i++) {
-      result.push(i);
-    }
-    return result;
-  };
+  // data range
+  const minDate = dayjs().add(6, 'minutes');
+  const maxDate = dayjs().add(48, 'hours');
 
-  const disabledDateTime = () => ({
-    disabledHours: () => range(0, 24).splice(0, dayjs().hour()),
-    disabledMinutes: () => range(0, 60).splice(0, dayjs().minute()),
-    disabledSeconds: () => range(0, 60).splice(0, dayjs().second())
-  });
+  const getRange = (start: number, end: number) => [...Array(end - start + 1).keys()].map((i) => i + start);
 
-  const disabledDate = (current: dayjs.Dayjs) => {
-    // Can not select days before today and now
-    return current && current < dayjs().startOf('day');
-  };
+  const disabledDate = useCallback(
+    (d: Dayjs) => {
+      if (d == null) {
+        return null;
+      }
+
+      return (
+        (minDate != null && d.isBefore(minDate) && !d.isSame(minDate, 'day')) ||
+        (maxDate != null && d.isAfter(maxDate) && !d.isSame(maxDate, 'day'))
+      );
+    },
+    [minDate, maxDate]
+  );
+
+  const disabledTime = useCallback(
+    (d: Dayjs) => {
+      if (d == null) {
+        return null;
+      }
+
+      if (d.isSame(minDate, 'day')) {
+        return {
+          disabledHours: () => (minDate.hour() > 0 ? getRange(0, minDate.hour() - 1) : []),
+          disabledMinutes: (hour: number) =>
+            hour === minDate.hour() && minDate.minute() > 0 ? getRange(0, minDate.minute() - 1) : []
+        };
+      }
+
+      if (d.isSame(maxDate, 'day')) {
+        return {
+          disabledHours: () => getRange(maxDate.hour() + 1, 24),
+          disabledMinutes: (hour: number) => (hour === maxDate.hour() ? getRange(maxDate.minute() + 1, 60) : [])
+        };
+      }
+
+      return null;
+    },
+    [minDate, maxDate]
+  );
 
   return (
     <AntdDatePicker
       showTime
-      placeholder="Now"
+      placeholder="Now + 6 minutes"
       onChange={onDatePickerChange}
       format="YYYY-MM-DD HH:mm:ss"
       disabledDate={disabledDate}
-      disabledTime={disabledDateTime}
+      disabledTime={disabledTime as any}
       value={dateValue ? dayjs(dateValue) : undefined}
     />
   );
